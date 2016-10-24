b4bfebfe1409f09cca93de9df4af73df6ca34960
cdll <cd234350150@gmail.com>
2016-10-24 20:35:35 +0800

    update test;

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
-<style lang="css" src="../style/home/home.css"></style>
+<!--<style lang="css" src="../style/home/home.css"></style>-->
+<style scoped>
+  .home {
+    font-size: 20Px;
+    width: 750px;
+    .main {
+      width: 550px;
+      height: 600px;
+      background: lightblue;
+      float: left;
+    }
+    .right {
+      width: 100px;
+      height: 600px;
+      background: lightpink;
+      float: left;
+    }
+  }
+</style>
 
 <template lang="html">
   <div class="home">
-    <div class="left" @click="leftClick">
-      left
-    </div>
+    <left-sidebar></left-sidebar>
     <div class="main">
       main
     </div>
@@ -15,25 +31,28 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {};
-  },
-  computed: {},
-  mounted () {},
-  methods: {
-    leftClick () {
-      const opts = {
-        url: 'https://cnodejs.org/api/v1/topics',
-        method: 'GET'
-      };
-      this.$store.dispatch('fetchApi', opts).then(() => {
-        console.log('fetch success');
-      });
+  import LeftSidebar from './leftSidebar.vue'
+  export default {
+    data () {
+      return {};
+    },
+    computed: {},
+    mounted () {},
+    methods: {
+      leftClick () {
+        const opts = {
+          url: 'https://cnodejs.org/api/v1/topics',
+          method: 'GET'
+        };
+        this.$store.dispatch('fetchApi', opts).then(() => {
+          console.log('fetch success');
+        });
 
-      this.$store.dispatch('increaseCount', 4);
+        this.$store.dispatch('increaseCount', 4);
+      }
+    },
+    components: {
+      LeftSidebar
     }
-  },
-  components: {}
-};
+  }
 </script>
---
+++ src/views/leftSidebar.vue
@@ -0,0 +1,23 @@
+<style scoped>
+  .left-sidebar{
+
+    width: 100px;
+    height: 600px;
+    background: lightgray;
+    float: left;
+  }
+</style>
+
+<template>
+  <section class="left-sidebar">{{content}}</section>
+</template>
+
+<script>
+  export default{
+    data() {
+      return {
+        content: 'left side bar'
+      }
+    }
+  }
+</script>
